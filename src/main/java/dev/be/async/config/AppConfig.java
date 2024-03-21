5fafc14ed9e723f9848a034d1a2e726353eb8c93
hgChoi <chlgusrms96@naver.com>
2024-03-21 17:43:23 +0900

    ThreadPool 정의

--- src/main/java/dev/be/async/config/AppConfig.java
+++ src/main/java/dev/be/async/config/AppConfig.java
@@ -1,9 +1,29 @@
 package dev.be.async.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 public class AppConfig {
     //Thread pool 정의
+    @Bean(name = "defaultTaskExecutor")
+    public ThreadPoolTaskExecutor defaultTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(50);
+
+        return executor;
+    }
+
+    @Bean(name = "messagingTaskExecutor", destroyMethod = "shutdown")
+    public ThreadPoolTaskExecutor messagingTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(20);
+        executor.setMaxPoolSize(50);
+
+        return executor;
+    }
 }
